5edd8cbe1429681021bb1e67eb1ec0b20c7209cc
tufankilicc <129671365+tufankilicc@users.noreply.github.com>
2023-08-13 10:10:38 +0300

    Update unlubaklava.py

--- unlubaklava.py
+++ unlubaklava.py
@@ -1,10 +1,1 @@
-import streamlit as st
 
-def main():
-    st.title("MR OGUZHAN BAYINDIR")
-    
-    if st.button("BUTONA TIKLA"):
-        st.write("AMCA BENİ AFFET".upper())
-
-if __name__ == "__main__":
-    main()
